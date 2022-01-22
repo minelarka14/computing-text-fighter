0bb2a126340477fd88a889c6551f2b069636fa11
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-22 22:52:36 +0800

    Update grid.py

--- grid.py
+++ grid.py
@@ -2,15 +2,6 @@ import os
 
 class DisplayGrid:
 
-    # def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
-    # def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
-    # def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
-    # def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
-    # def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
-    # def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
-    # def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
-    # def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
-
     def __init__(self, xl: int, yl: int, abilities, pos1=(0, 0), pos2=(1, 1), sHealth:int = 100):
         self.xLength = xl
         self.yLength = yl
@@ -27,8 +18,8 @@ class DisplayGrid:
         │      {}                                              {}         │
         │                                                                        │
         │       {}                                               {}        │
-        │      {}                                         {}       │
-        │      {}                                        {}       │
+        │      {}                                         {}       |
+        |      {}                                        {}       │
         │       \_-_/_                                       / /    \_-_/_       │
         │    .-"`'V'//-.                                     \ `-.-"`'V'//-.     │
         │   / ,   |// , \\                                     `.__,   |// , \\    │
